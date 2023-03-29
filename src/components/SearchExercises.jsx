098e8d599bc2576cc00a891ee09ed4d20608e03e
gia <giadatdoan@gmail.com>
2023-03-29 16:32:04 -0400

    added api

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -2,15 +2,36 @@ import React, { useEffect, useState } from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 import { fetchData, exerciseOptions } from '../utils';
+import { HorizontalScrollbar } from './index';
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState('');
+  const [bodyParts, setBodyParts] = useState([])
+  
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+      setBodyParts(['all', ...bodyPartsData]);
+    }
+
+    fetchExercisesData();
+  }, [])
+  
 
   const handleSearch = async () => {
     if(search) {
-      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
     
-      console.log('data', exercisesData);
+      const searchedExercised = exercisesData.filter((exercise) =>
+        exercise.name.toLowerCase.includes(search)
+        || exercise.target.toLowerCase.includes(search)
+        || exercise.equipment.toLowerCase.includes(search)
+        || exercise.bodyPart.toLowerCase.includes(search)
+      );
+
+      setSearch('');
+      setExercises(searchedExercised);
     }
   }
 
@@ -58,6 +79,16 @@ const SearchExercises = () => {
         >
           Search
         </Button>
+      </Box>
+
+      <Box
+        sx={{
+          position: 'relative',
+          width: '100%',
+          p: '20px'
+        }}
+      >
+        <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart} />
       </Box>
     </Stack>
   )
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,15 @@ import Footer from "./Footer";
 import HeroBanner from "./HeroBanner";
 import SearchExercises from "./SearchExercises";
 import Exercises from "./Exercises";
+import HorizontalScrollbar from "./HorizontalScrollbar";
+import BodyPart from "./BodyPart";
 
-export { Navbar, Footer, HeroBanner, SearchExercises, Exercises };
+export {
+  Navbar,
+  Footer,
+  HeroBanner,
+  SearchExercises,
+  Exercises,
+  HorizontalScrollbar,
+  BodyPart,
+};
